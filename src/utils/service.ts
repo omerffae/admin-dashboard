e6ed3388f25d98ced6d25065164df05ed3b43664
omerffae <omerekim825@gmail.com>
2025-06-09 12:05:03 +0300

    first commit

---
+++ src/utils/service.ts
@@ -0,0 +1,31 @@
+import { GetOrdersResponse, GetProductsResponse, Product } from "@/types";
+
+const API_URL = "http://localhost:4000";
+
+const getOrders = async (): GetOrdersResponse => {
+  const response = await fetch(`${API_URL}/orders`);
+
+  return response.json();
+};
+
+const getProducts = async (): GetProductsResponse => {
+  const response = await fetch(`${API_URL}/products`);
+
+  return response.json();
+};
+
+const getProduct = async (productId: string): Promise<Product> => {
+  const response = await fetch(`${API_URL}/products/${productId}`);
+
+  return response.json();
+};
+
+const deleteProduct = async (productId: string) => {
+  const response = await fetch(`${API_URL}/products/${productId}`, {
+    method: "DELETE",
+  });
+
+  return response.json();
+};
+
+export { getOrders, getProducts, deleteProduct, getProduct };
